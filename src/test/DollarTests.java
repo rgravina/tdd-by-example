27658578c02b5dd7981a605bf335aabe955c2b79
Robert Gravina <rgravina@pivotal.io>
2018-05-17 17:30:00 +0900

    Use Dollar in test assertions

--- src/test/DollarTests.java
+++ src/test/DollarTests.java
@@ -8,14 +8,14 @@ class DollarTests {
     @Test
     void testMultiplication() {
         Dollar five = new Dollar(5);
-        assertEquals(10, five.times(2).amount);
+        assertEquals(new Dollar(10), five.times(2));
     }
 
     @Test
     void testMultiplicationMultipleTimes() {
         Dollar five = new Dollar(5);
-        assertEquals(10, five.times(2).amount);
-        assertEquals(15, five.times(3).amount);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
     }
 
     @Test
